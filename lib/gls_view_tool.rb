f77ef2354b1b2be4db46f89cd813d5c6827f60b4
John Stegman <john@green-leaf-services.com>
2017-04-19 11:22:19 -0400

    GLS View Tool initial commit

---
+++ lib/gls_view_tool.rb
@@ -0,0 +1,6 @@
+require "gls_view_tool/version"
+require "gls_view_tool/renderer"
+
+module GlsViewTool
+  # Your code goes here...
+end
---
+++ lib/gls_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GlsViewTool
+  VERSION = "0.1.0"
+end
